62bd89a2eca4a8a51c3c0c35cf9f8100d0cc51a9
nicksfdev <nicksfdev@gmail.com>
2025-11-11 09:00:43 -0800

    Added onSelect hook to the table raw.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import './App.css';
-import PropTypes from 'prop-types';
-import pokemon from "./pokemon.json"
+import "./App.css";
+import PropTypes from "prop-types";
+import pokemon from "./pokemon.json";
 
-const PokemonRow = ({ pokemon }) => (
+const PokemonRow = ({ pokemon, onSelect }) => (
   <tr key={pokemon.id}>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select!</button>
+    </td>
   </tr>
 );
 
@@ -17,24 +20,28 @@ PokemonRow.propTypes = {
     }),
     type: PropTypes.arrayOf(PropTypes.string),
   }),
+  onSelect: PropTypes.func,
 };
 
 function App() {
   const [filter, filterSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState(null);
   return (
     <div
       style={{
         margin: "auto",
-        width: '500px',
+        width: "500px",
         paddingTop: "1rem",
       }}
     >
       <h1 className="title">Business List</h1>
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "70% 30%",
-        gridColumnGap: "1rem",
-      }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          gridColumnGap: "1rem",
+        }}
+      >
         <div>
           <input
             value={filter}
@@ -49,17 +56,29 @@ function App() {
             </thead>
             <tbody>
               {pokemon
-                .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
                 .slice(0, 20)
                 .map((pokemon) => (
-                  <PokemonRow pokemon={pokemon} key={pokemon.id} />
+                  <PokemonRow
+                   pokemon={pokemon}
+                   key={pokemon.id}
+                   onSelect={(pokemon) => selectedItemSet(pokemon)} />
                 ))}
             </tbody>
           </table>
         </div>
+        {selectedItem && (
+          <div>
+            <h1>{selectedItem.name.english}</h1>
+          </div>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default App;
